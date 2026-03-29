8412b1343dd3b1cd79576c21f9d7988fbe0e6fb1
yanyonghao <2099130598@qq.com>
2026-03-29 22:48:06 +0800

    登录功能实现

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import java.time.LocalDateTime;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -15,6 +29,75 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送手机验证码
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.验证手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        // 3.如果符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        // 4.保存验证码到session
+        session.setAttribute("code", code);
+        // 5.发送验证码
+        log.info("发送短信验证码：" + code);
+
+        //返回ok
+        return Result.ok();
+    }
+
+    /**
+     * 登录功能
+     */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)){
+            // 如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+
+        // 2.校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (code==null || !code.equals(cacheCode.toString())){
+            // 不一致，报错
+            return Result.fail("验证码错误！");
+        }
+
+        // 3.根据手机号到数据库查询用户是否存在  select * from user where phone = ?
+        User user = query().eq("phone", phone).one();
+
+        // 4.如果没有，就创建用户并保存到数据库
+        if (user==null){
+            user=createUserWithPhone(phone);
+        }
+
+        // 5.保存用户信息到session
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+        return Result.ok();
+    }
+
+    /**
+     *  根据手机号创建用户
+     */
+    private User createUserWithPhone(String phone) {
+        // 1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateTime(LocalDateTime.now());
+        //2.保存用户
+        save(user);
+        return user;
+    }
 }
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.从session里获取用户信息
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("user");
+        // 2.判断用户是否存在
+        if (user==null){
+            // 3.如果不存在，拦截 返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+        // 4.存在，保存用户信息到ThreadLocal
+        UserHolder.saveUser((UserDTO) user);
+        // 5.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 移除用户
+        UserHolder.removeUser();
+    }
+}
